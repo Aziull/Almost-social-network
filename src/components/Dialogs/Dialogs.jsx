bade638577405b4f5d2253269993596246848c30
Aziull <65764457+Aziull@users.noreply.github.com>
2020-09-11 22:31:57 +0300

    modified

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,31 +2,37 @@ import React from 'react'
 import s from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem'
 import Messages from './Messages/Messages'
-
-const Dialogs = (props) => {
-
-    let addMessage = () =>{
-        props.addMessage()
+import { render } from '@testing-library/react'
+ 
+class Dialogs extends React.Component{
+    constructor(props){
+        super(props)
+        
     }
-    let onMessageChange = (e) => {
+    addMessage = () =>{
+        this.props.addMessage()
+    }
+    onMessageChange = (e) => {
         let text = e.target.value
-        props.updateNewMessageText(text)
+        this.props.updateNewMessageText(text)
     }
  
-let dialogsArray = props.dialogsPage.dialogs.map(elem => <DialogItem name={elem.name} id={elem.id} key={elem.id} />)
-let messagesArray = props.dialogsPage.messages.map(elem => <Messages message={elem.message} key={elem.id}/>)
-
-    return (
-        <div className={s.dialogs}>
+render(){
+    let dialogsArray = this.props.dialogsPage.dialogs.map(elem => <DialogItem name={elem.name} id={elem.id} key={elem.id} />)
+    let messagesArray = this.props.dialogsPage.messages.map(elem => <Messages message={elem.message} key={elem.id}/>)
+     return  <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsArray}
             </div>
             <div className={s.messages}>
                 {messagesArray}
-                <textarea onChange={onMessageChange} value={props.newMessageText} placeholder="введи шось"/> <br/>
-                <button onClick={addMessage}>Add Message</button>
+                <textarea onChange={this.onMessageChange} value={this.props.newMessageText} placeholder="введи шось"/> <br/>
+                <button onClick={this.addMessage}>Add Message</button>
             </div>
         </div>
-    );
+    
 }
+   
+}
+
 export default Dialogs;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,46 +3,48 @@ import s from './Users.module.css'
 import * as axios from 'axios'
 import userPhoto from '../../assets/images/avatar.jpg'
 
-const Users = (props) => {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-    let getUsers = () => {
 
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-
-                props.setUsers(response.data.items)
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
             })
-        }
 
     }
 
-    return (<div>
-        <button onClick={getUsers}>Get Users</button>
-        {props.users.map(u => <div key={u.Id}>
-            
-            <span>
-                <div>
-                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.usersPhoto} alt='' />
-                </div>
-                <div>
-                    {u.follows ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
-                        : <button onClick={() => { props.follow(u.id) }} >Follow</button>}
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.Id}>
 
+                    <span>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.usersPhoto} alt='' />
+                        </div>
+                        <div>
+                            {u.follows ? <button onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button>
+                                : <button onClick={() => { this.props.follow(u.id) }} >Follow</button>}
+
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
+                    </span>
                 </div>
-            </span>
-            <span>
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-                <span>
-                    <div>{"u.location.country"}</div>
-                    <div>{"u.location.city"}</div>
-                </span>
-            </span>
+                )
+            }
         </div>
-        )}
-         </div>
-   )
+
+    }
 }
 export default Users
